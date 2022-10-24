6ff3c66dca7cf64fc276e1af313b4096dd90d8ea
Robert Mourey Jr <rmourey_jr@quantumone.network>
2022-10-24 14:46:48 -0400

    eval-overrides

--- overlays/haskell-nix-extra/utils/default.nix
+++ overlays/haskell-nix-extra/utils/default.nix
@@ -1,15 +1,20 @@
 { lib, haskell-nix, symlinkJoin
-, compiler-nix-name, index-state }:
+, compiler-nix-name, index-state
+, evalSystem ? null }:
 
 let
   project = mkProject {};
+  
+  evalSystemArg = if evalSystem == null
+     then {}
+     else { inhert evalSystem; };
   mkProject = args: haskell-nix.cabalProject ({
     src = haskell-nix.haskellLib.cleanSourceWith {
       name = "tbco-nix-utils";
       src = ./.;
     };
     inherit compiler-nix-name index-state;
-  } // args);
+  } // evalSystemArg // args);
 in
   symlinkJoin {
     name = "tbco-nix-utils";
